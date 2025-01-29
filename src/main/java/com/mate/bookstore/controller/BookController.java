a4a567ff584d90e1b0ee87ee239b3e079bd88fd4
smagles <lesialist@icloud.com>
2025-01-29 11:44:28 +0100

    added validation in CreateBookRequestDto

--- src/main/java/com/mate/bookstore/controller/BookController.java
+++ src/main/java/com/mate/bookstore/controller/BookController.java
@@ -4,12 +4,16 @@ import com.mate.bookstore.dto.BookDto;
 import com.mate.bookstore.dto.CreateBookRequestDto;
 import com.mate.bookstore.service.BookService;
 import java.util.List;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -19,17 +23,20 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<BookDto> getAll() {
         return bookService.findAll();
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto createBookRequestDto) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto createBookRequestDto) {
         return bookService.save(createBookRequestDto);
     }
 }
--- src/main/java/com/mate/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/mate/bookstore/dto/CreateBookRequestDto.java
@@ -1,14 +1,25 @@
 package com.mate.bookstore.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank(message = "Title is required")
     private String title;
+
+    @NotBlank(message = "Author is required")
     private String author;
+
     private String isbn;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be positive")
     private BigDecimal price;
+
     private String description;
     private String coverImage;
 }
