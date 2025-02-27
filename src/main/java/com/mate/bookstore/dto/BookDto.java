7855acb34f5a7e635cfd46bda2991a020b456cf7
smagles <lesialist@icloud.com>
2025-02-27 15:26:58 +0100

    added validation annotations to dto

--- src/main/java/com/mate/bookstore/dto/BookDto.java
+++ src/main/java/com/mate/bookstore/dto/BookDto.java
@@ -1,21 +1,28 @@
 package com.mate.bookstore.dto;
 
 import java.math.BigDecimal;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class BookDto {
     private Long id;
 
+    @NotBlank(message = "Title is required")
     private String title;
 
+    @NotBlank(message = "Author is required")
     private String author;
 
+    @NotBlank(message = "isbn is required")
     private String isbn;
 
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be positive")
     private BigDecimal price;
 
     private String description;
-
     private String coverImage;
 }
