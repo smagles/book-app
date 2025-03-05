38e3b408ec3e8687242651539c06fb5e793f06a2
smagles <lesialist@icloud.com>
2025-03-05 12:47:46 +0100

    Added validation Pageable

--- src/main/java/com/mate/bookstore/controller/BookController.java
+++ src/main/java/com/mate/bookstore/controller/BookController.java
@@ -9,6 +9,8 @@ import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +19,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,8 +30,8 @@ public class BookController implements BookApi {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public List<BookDto> getAll(Pageable pageable) {
+    public List<BookDto> getAll(@PageableDefault(page = 0, size = 10,
+            sort = "title", direction = Sort.Direction.ASC) Pageable pageable) {
         return bookService.findAll(pageable);
     }
 
@@ -61,7 +62,8 @@ public class BookController implements BookApi {
 
     @GetMapping("/search")
     @ResponseStatus(HttpStatus.OK)
-    public List<BookDto> searchBooks(BookSearchParametersDto searchParameters, Pageable pageable) {
+    public List<BookDto> searchBooks(BookSearchParametersDto searchParameters,
+                                     @PageableDefault(page = 0, size = 10) Pageable pageable) {
         return bookService.searchBooks(searchParameters, pageable);
     }
 }
