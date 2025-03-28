5bddc22a3664e8fe5ecac880360983cc4db3c9cf
smagles <lesialist@icloud.com>
2025-03-28 21:01:46 +0100

    fix validation message

--- src/main/java/com/mate/bookstore/security/CustomUserDetailsService.java
+++ src/main/java/com/mate/bookstore/security/CustomUserDetailsService.java
@@ -15,7 +15,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email).orElseThrow(()
-                -> new UsernameNotFoundException("Can't find user by email" + email));
+                -> new UsernameNotFoundException("Can't find user by email " + email));
 
     }
 }
