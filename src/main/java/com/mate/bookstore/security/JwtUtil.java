a00b6b79b1450c2e1878df88c6ffc56b68b1171b
smagles <lesialist@icloud.com>
2025-04-01 16:42:10 +0200

    fix jwt secret key

--- src/main/java/com/mate/bookstore/security/JwtUtil.java
+++ src/main/java/com/mate/bookstore/security/JwtUtil.java
@@ -3,6 +3,7 @@ package com.mate.bookstore.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
@@ -14,17 +15,20 @@ import org.springframework.stereotype.Component;
 public class JwtUtil {
     @Value("${jwt.expiration}")
     private long expiration;
-    private final Key secret;
+    @Value("${jwt.secret}")
+    private String secretString;
+    private Key secret;
 
-    public JwtUtil(@Value("${jwt.secret}") String secretString) {
+    @PostConstruct
+    public void init() {
         this.secret = Keys.hmacShaKeyFor(secretString.getBytes(StandardCharsets.UTF_8));
     }
 
     public String generateToken(String username) {
         return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(secret)
                 .compact();
     }
