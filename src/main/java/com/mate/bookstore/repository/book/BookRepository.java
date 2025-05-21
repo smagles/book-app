5f3fee18dd3baebc175ac7456162be29cbc628a2
smagles <lesialist@icloud.com>
2025-05-21 17:42:22 +0200

    fix problem n+1

--- src/main/java/com/mate/bookstore/repository/book/BookRepository.java
+++ src/main/java/com/mate/bookstore/repository/book/BookRepository.java
@@ -1,9 +1,10 @@
 package com.mate.bookstore.repository.book;
 
 import com.mate.bookstore.model.Book;
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -11,14 +12,15 @@ import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<Book, Long>,
         JpaSpecificationExecutor<Book> {
-    Optional<Book> findById(Long id);
-
-    Book save(Book book);
-
-    void deleteById(Long id);
 
     boolean existsByIsbn(String isbn);
 
     @Query("SELECT b FROM Book b JOIN b.categories c WHERE c.id = :categoryId")
     Page<Book> findAllByCategoryId(@Param("categoryId") Long categoryId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"categories"})
+    Page<Book> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = {"categories"})
+    Page<Book> findAll(Specification specification, Pageable pageable);
 }
